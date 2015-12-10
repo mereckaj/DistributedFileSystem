a3e4afc23cb00bbfbea1984375e6cd050a21d63f
Julius Mereckas <mereckaj@tcd.ie>
2015-12-10 19:22:29 +0000

    New start

--- Authentication/src/com/mereckas/dfs/authentication/client/AuthClientTest.java
+++
@@ -1,8 +0,0 @@
-package com.mereckas.dfs.authentication.client;
-public class AuthClientTest {
-	public static void main(String[] args){
-		AuthenticationClient authenticationClient = new AuthenticationClient("0.0.0.0",8000,"Test");
-		authenticationClient.connect();
-		authenticationClient.authenticate();
-	}
-}
--- Authentication/src/com/mereckas/dfs/shared/ErrorReporter.java
+++
@@ -1,7 +0,0 @@
-package com.mereckas.dfs.shared;
-
-public class ErrorReporter {
-	public static void logError(String message,String error,int lineNumber){
-		System.err.println(message + " | " + error + " | " + "line: " + lineNumber);
-	}
-}
